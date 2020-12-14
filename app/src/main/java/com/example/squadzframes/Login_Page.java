bc76ef5184040fe1fb532a42e55a8c549dc172c2
David Pham <david.phamgm@gmail.com>
2020-12-13 22:13:36 -0500

    Things that were added2: a shared pref thing for the remember me on login page and firebase sending the user object that supposedly should work

--- app/src/main/java/com/example/squadzframes/Login_Page.java
+++ app/src/main/java/com/example/squadzframes/Login_Page.java
@@ -3,24 +3,78 @@ package com.example.squadzframes;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+import android.widget.Switch;
+import android.widget.Toast;
 
 public class Login_Page extends AppCompatActivity {
+
+    private EditText username, password;
+    private Switch switch1;
+
+    private String textUser, textPass;
+    private boolean switchOnOff;
+    public static final String SHARED_PREFs = "sharedPrefs";
+    public static final String TEXT = "text";
+    public static final String TEXTPASS = "textPass";
+    public static final String SWITCH1 = "switch1";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login__page);
         Button login_button = findViewById(R.id.login_button);
-        Button sign_up_button  = findViewById(R.id.sign_up_button);
+        Button sign_up_button = findViewById(R.id.sign_up_button);
+
+        SharedPreferences preferences = getSharedPreferences("checkbox", MODE_PRIVATE);
+        String checkbox = preferences.getString("remember", "");
+        if (checkbox.equals("true")) {
+            Intent send = new Intent(Login_Page.this, MainActivity.class);
+            startActivity(send);
+        } else {
+            Toast.makeText(Login_Page.this, "Please Sign in", Toast.LENGTH_SHORT).show();
+        }
+
+        //initialize the views that we want to keep
+        username = (EditText) findViewById(R.id.editUserName);
+        password = (EditText) findViewById(R.id.loginPassword);
+        switch1 = (Switch) findViewById(R.id.switch1);
 
         login_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent send = new Intent(Login_Page.this, MainActivity.class);
+
+                // username.setText(username.getText().toString());
+                //password.setText((password.getText().toString()));
+                // loadData();
+                //updateViews();
                 startActivity(send);
+            }
+        });
+
+        switch1.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (buttonView.isChecked()) {
+                    SharedPreferences preferences = getSharedPreferences("checkbox", MODE_PRIVATE);
+                    SharedPreferences.Editor editor = preferences.edit();
+                    editor.putString("remember", "true");
+                    editor.apply();
+                    Toast.makeText(Login_Page.this, "Checked", Toast.LENGTH_SHORT).show();
+                } else if (!buttonView.isChecked()) {
+                    SharedPreferences preferences = getSharedPreferences("checkbox", MODE_PRIVATE);
+                    SharedPreferences.Editor editor = preferences.edit();
+                    editor.putString("remember", "false");
+                    editor.apply();
+                    Toast.makeText(Login_Page.this, "Unchecked", Toast.LENGTH_SHORT).show();
+
+                }
             }
         });
 
@@ -32,4 +86,27 @@ public class Login_Page extends AppCompatActivity {
             }
         });
     }
+
+//    public void saveData(){
+//        SharedPreferences sharedPreferences = getSharedPreferences(SHARED_PREFs,MODE_PRIVATE);
+//        SharedPreferences.Editor editor = sharedPreferences.edit();
+//        editor.putString(TEXT,username.getText().toString());
+//        editor.putString(TEXTPASS,password.getText().toString());
+//        editor.putBoolean(SWITCH1,switch1.isChecked());
+//
+//        editor.apply();
+//        Toast.makeText(this,"data saved",Toast.LENGTH_SHORT).show();
+//    }
+//
+//    public void loadData(){
+//        SharedPreferences sharedPreferences = getSharedPreferences(SHARED_PREFs,MODE_PRIVATE);
+//        textUser = sharedPreferences.getString(TEXT,"");
+//        textPass = sharedPreferences.getString(TEXTPASS,"");
+//        switchOnOff = sharedPreferences.getBoolean(SWITCH1,false);
+//    }
+//
+//    public void updateViews(){
+//     username.setText(textUser);
+//     password.setText(textPass);
+//    }
 }
--- app/src/main/java/com/example/squadzframes/SignUpPage.java
+++ app/src/main/java/com/example/squadzframes/SignUpPage.java
@@ -1,31 +1,71 @@
 package com.example.squadzframes;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
-import com.example.squadzframes.ui.home.HomeFragment;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.squadzframes.model.UserInformation;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+//import com.example.firebaseapp.model.UserInformation;
+//import com.example.squadzframes.firebase.realTimeDatabase.RealtimeDatabaseManager;
+
 
 public class SignUpPage extends AppCompatActivity {
+    EditText username, fullName, email, password, date;
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+    DatabaseReference databaseReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up_page);
 
+        databaseReference = database.getReference("User Information");
+
+        username = findViewById(R.id.editTextUsername);
+        fullName = findViewById(R.id.editTextPersonName);
+        email = findViewById(R.id.editTextEmailAddress);
+        password = findViewById(R.id.editTextPassword);
+        date = findViewById(R.id.editTextDate);
+
+
         Button btn = (Button) findViewById(R.id.sign_Up_Button);
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                sendData();
                 goHome();
             }
         });
     }
-    public void goHome(){
+
+    private void sendData() {
+        String sUsername, sFullname, sEmail, sPassword, sDate;
+
+        sUsername = username.getText().toString();
+        sFullname = fullName.getText().toString();
+        sEmail = email.getText().toString();
+        sPassword = password.getText().toString();
+        sDate = date.getText().toString();
+
+        String id = databaseReference.push().getKey();
+
+        UserInformation personInfo = new UserInformation(sUsername, sFullname, sEmail, sPassword, sDate);
+
+        databaseReference.child(id).setValue(personInfo);
+
+    }
+
+    public void goHome() {
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
     }
+
+
 }
